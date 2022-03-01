7362dc7c74386bf4b93b5393240a44e804448175
Linda Chen <lindachen@Lindas-MacBook-Pro-2.local>
2022-03-01 15:42:30 -0500

    added more styling to modal

--- pokedex/src/components/MoveModal.js
+++ pokedex/src/components/MoveModal.js
@@ -6,64 +6,30 @@ const StyledDiv = styled.div`
   justify-content: end;
 `;
 
+const AttributeDiv = styled.div`
+  text-align: justify;
+  margin-top: 8px;
+`;
+
 const StyledModal = styled.div`
-  width: 500px;
+  display: flex;
+  align-content: left;
+  position: absolute;
+  top: 25%;
+  max-height: 300px;
+  width: 400px;
+  overflow-y: scroll;
+  transform: translate(-50%, -50%);
   background: white;
   border: 2px solid #ccc;
   border-radius: 4px;
+  padding: 12px;
   transition: 1.1s ease-out;
   box-shadow: -2rem 2rem 2rem rgba(black, 0.2);
   filter: blur(0);
   transform: scale(1);
   opacity: 1;
   visibility: visible;
-
-  &.red {
-    background: black;
-    color: red;
-  }
-  &.blue {
-    background: blue;
-    color: red;
-  }
-
-  &.off {
-    opacity: 0;
-    visibility: hidden;
-    filter: blur(8px);
-    transform: scale(0.33);
-    box-shadow: 1rem 0 0 rgba(black, 0.2);
-  }
-  @supports (offset-rotate: 0deg) {
-    offset-rotate: 0deg;
-    &.off {
-      offset-distance: 100%;
-    }
-  }
-  @media (prefers-reduced-motion) {
-    offset-path: none;
-  }
-  h2 {
-    border-bottom: 1px solid #ccc;
-    padding: 1rem;
-    margin: 0;
-  }
-  .content {
-    padding: 1rem;
-  }
-  .actions {
-    border-top: 1px solid #ccc;
-    background: #eee;
-    padding: 0.5rem 1rem;
-    button {
-      border: 0;
-      background: #78f89f;
-      border-radius: 5px;
-      padding: 0.5rem 1rem;
-      font-size: 0.8rem;
-      line-height: 1;
-    }
-  }
 `;
 
 export const MoveModal = ({ moveMetadata, modalOpen, toggleModalOpen }) => {
@@ -79,10 +45,10 @@ export const MoveModal = ({ moveMetadata, modalOpen, toggleModalOpen }) => {
             <button onClick={() => toggleModalOpen()}>Close</button>
           </StyledDiv>
           <div>
-            <p>Skill Name: {moveMetadata?.name}</p>
-            <p>Effect: {getMoveEffect()}</p>
-            <p>Accuracy: {moveMetadata?.accuracy || 0}</p>
-            <p>PP: {moveMetadata?.pp}</p>
+            <AttributeDiv>Skill Name: {moveMetadata?.name}</AttributeDiv>
+            <AttributeDiv>Effect: {getMoveEffect()}</AttributeDiv>
+            <AttributeDiv>Accuracy: {moveMetadata?.accuracy || 0}</AttributeDiv>
+            <AttributeDiv>PP: {moveMetadata?.pp}</AttributeDiv>
           </div>
         </div>
       </StyledModal>
