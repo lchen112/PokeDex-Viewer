36bf8d0be487e50b51bbab0680d9914f3ae2791a
Linda Chen <lindachen@Lindas-MacBook-Pro-2.local>
2022-03-01 12:34:42 -0500

    added styling to types

--- pokedex/src/components/PokemonTypes.js
+++ pokedex/src/components/PokemonTypes.js
@@ -2,10 +2,14 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const StyledButton = styled.button`
-  background-color: black;
-  font-size: 16px;
-  color: white;
-  margin-left: 8px;
+  color: palevioletred;
+  font-size: 1em;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  color: tomato;
+  border-color: tomato;
 `;
 
 const StyledDiv = styled.div`
